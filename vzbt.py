bd2c724036a5b0936a5852a1bcd5a7f4617e0c3a
Andrey Abushek <Andrey Abushek>
2017-06-01 22:53:53 +0300

    'v9'

--- vzbt.py
+++ vzbt.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 import requests
 import telebot
+import time
 
 token = '286099721:AAEosVONIGX_DMoaxJIGiqSUEx296uFhhiI'
 bot = telebot.TeleBot(token)
@@ -50,8 +51,9 @@ def booking_now(url):
 
 
 def booking_all(url):
+    bl = []
+    boking_list = []
     count = 0
-    now = datetime.now().timestamp()
     url_request = urllib.request.urlopen(url)
     url_read = url_request.read()
     soup = BeautifulSoup(url_read, "html.parser")
@@ -61,29 +63,36 @@ def booking_all(url):
             text = (str(t[i])[str(t[i]).find('[')+1:str(t[i]).find(']')])
             l = text[1:-1].split('},{')
             for i in l:
-                start_time = int(i[i.find('datetime')+11:i.find(',"datetime_no_tz"')-1])
-                start_day = datetime.fromtimestamp(int(start_time)).strftime('%Y-%m-%d')
-                today = datetime.fromtimestamp(int(now)).strftime('%Y-%m-%d')
-                if start_day == today:
                     if (i[i.find('status')+9:i.find('datetime')-3] != 'closed') & (i[i.find('status')+9:i.find('datetime')-3] != 'open'):
+                        boking_list.append(i)
                         count+=1
 
-    return count
+    for i in range (len(boking_list)):
+        ts = boking_list[i].split(',')[2].split(':')[1].replace('"','')
+        bl.append(time.ctime(int(ts)))
+
+
+    return bl
+
+
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
     # bot.send_message(message.chat.id, message.text)
     # user_markup
 
-    if message.text == 'crime':
-        text = 'Идеальное преступление: ' + str(booking_now('http://vzaperti.com.ua/quest/crime'))
-        bot.send_message(message.chat.id, text)
-    elif message.text == 'empire':
-        text = 'Подпольная империя: ' + str(booking_now('http://vzaperti.com.ua/quest/empire'))
-        bot.send_message(message.chat.id, text)
+    # if message.text == 'crime':
+    #     text = 'Идеальное преступление: ' + str(booking_now('http://vzaperti.com.ua/quest/crime'))
+    #     bot.send_message(message.chat.id, text)
+    # elif message.text == 'empire':
+    #     text = 'Подпольная империя: ' + str(booking_now('http://vzaperti.com.ua/quest/empire'))
+    #     bot.send_message(message.chat.id, text)
 
-    elif message.text =='count':
-        text = 'Количество броней \nПодпольная империя: '+ str(booking_all('http://vzaperti.com.ua/quest/empire'))+'\nИдеальное преступление: '+str(booking_all('http://vzaperti.com.ua/quest/crime'))
+    if message.text =='count':
+        # str(booking_all('http://vzaperti.com.ua/quest/empire'))
+        bokking_empire = str(booking_all('http://vzaperti.com.ua/quest/empire')).replace("2017'", '\n').replace("'", '').replace('[','').replace(', ', '').replace(']', '')
+        bokking_crime = str(booking_all('http://vzaperti.com.ua/quest/crime')).replace("2017'", '\n').replace("'",'').replace('[', '').replace(', ', '').replace(']', '')
+        text = 'Количество броней \nПодпольная империя:\n '+ bokking_empire+'\nИдеальное преступление:\n '+bokking_crime
         bot.send_message(message.chat.id, text)
     else:
         text = 'ну'
